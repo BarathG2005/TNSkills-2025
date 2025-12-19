8a696835b54df02de6ccbfd5a3594b3e9e1a8878
BarathG2005 <158817182+BarathG2005@users.noreply.github.com>
2025-12-19 16:19:02 +0530

    implement debounce

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,12 +1,22 @@
+import { headers } from "next/headers";
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 export default function Home() {
+  const [data,setData] = useState();
   useEffect(()=>{
     const fetch =async()=> {
-      const res = await fetch("https://localhost:")
-    }
-    return fetch()
-  },[])
+      const res = await fetch("https://localhost:8080",{
+        methods:"GET",
+        headers:{
+          "content-Type":"application/json",
+        }
+      })
+    const data = await res.json();
+    return data
+  }
+    setData(fetch())
+  
+},[])
   return (
     <div> </div>
   )
