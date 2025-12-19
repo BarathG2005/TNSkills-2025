757c9dab13f86c8f23e9bc4c7693ae7ae53b0bf5
BarathG2005 <158817182+BarathG2005@users.noreply.github.com>
2025-12-19 14:17:11 +0530

    schemas setup

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,6 @@
 from fastapi import FastAPI
 from config.database import get_db
 app = FastAPI()
-
-
-   
-
-
 @app.get("/")
 def root():
     return {
---
+++ backend/query.sql
@@ -0,0 +1,47 @@
+create table Vehicles(
+	VehicleID int primary key,
+	RegNo varchar(200),
+	Type varchar(200),
+	LastServiceDate Date,
+	CurrentOdometer long,
+	LastServiceOdometer long
+);
+
+create table Trips(
+	TripID int primary key,
+	VehicleID int ,
+    DriverName varchar(200),
+    StartDate date,
+    EndDate date,
+    DistanceKm long,
+    
+    constraint fk_veh
+    foreign key  Vehicles(VehicleID)
+    references  Vehicles(VehicleID)
+);
+
+
+create table  Service_History(
+	ServiceID int primary key,
+	VehicleID int ,
+	ServiceDat date,
+    OdometerReading long,
+    Notes varchar(200),
+    
+    constraint ser_fk
+    foreign key  Vehicles(VehicleID)
+    references  Vehicles(VehicleID)
+);
+
+create table  Maintenance_Alerts(
+	AlertID int primary key,
+	VehicleID int ,
+	AlertDate date,
+    Reason varchar(500),
+    
+    constraint m_fk
+    foreign key  Vehicles(VehicleID)
+    references  Vehicles(VehicleID)
+);
+
+
